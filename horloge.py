e782b0d735102edf03ca8e6af9dbaecad90ecb4f
Teddy Baccam <144665060+thouay-baccam@users.noreply.github.com>
2023-11-29 10:35:58 +0100

    MaJ

--- horloge.py
+++ horloge.py
@@ -1,36 +1,38 @@
 import msvcrt
 from time import sleep, time
 
-pause = False
+def toggle_pause(est_en_pause):
+    return not est_en_pause
 
-def toggle_pause():
-    global pause
-    pause = not pause
-    print("\nL'horloge est en pause" if pause else "\nReprise de l'horloge")
+def obtenir_type_horloge():
+    while True:
+        type_horloge = input("Entrez le type d'affichage de l'heure (AMPM/24H) : ").upper()
+        if type_horloge in ['AMPM', '24H']:
+            return type_horloge
+        else:
+            print("Veuillez entrer une option valide (AMPM/24H).")
 
-def pm_or_24h():
-    clock_type = input("Entrez le type d'affichage de l'heure (AMPM/24H) : ")
-    return clock_type.upper()
-
-def dalarme(heure_alarme, minute_alarme, seconde_alarme, heure, minute, seconde):
+def alarme(heure_alarme, minute_alarme, seconde_alarme, heure, minute, seconde):
     if heure_alarme == heure and minute_alarme == minute and seconde_alarme == seconde:
         print("\nL'alarme a sonné")
 
-def afficher_heure(heure_base, alarme):
+def afficher_heure(heure_base, heure_alarme, est_en_pause):
     heure, minute, seconde = heure_base
-    heure_alarme, minute_alarme, seconde_alarme = alarme
+    heure_alarme, minute_alarme, seconde_alarme = heure_alarme
     dernier_temps = time()
-    affichage = pm_or_24h()
+    affichage = obtenir_type_horloge()
 
     while True:
         if msvcrt.kbhit():
-            key = msvcrt.getch().decode('utf-8')
-            if key.lower() == 'p':
-                toggle_pause()
+            touche = msvcrt.getch().decode('utf-8')
+            if touche.lower() == 'p':
+                est_en_pause = toggle_pause(est_en_pause)
 
-        if pause:
+        if est_en_pause:
             sleep(1)
             continue
+
+        alarme(heure_alarme, minute_alarme, seconde_alarme, heure, minute, seconde)  # Vérifier l'alarme en premier
 
         temps_actuel = time()
         temps_ecoule = temps_actuel - dernier_temps
@@ -38,30 +40,34 @@ def afficher_heure(heure_base, alarme):
         if temps_ecoule >= 1:
             seconde += 1
             dernier_temps = temps_actuel
+
             if seconde >= 60:
                 minute += seconde // 60
                 seconde %= 60
+
                 if minute >= 60:
                     heure += minute // 60
                     minute %= 60
+
                     if heure >= 24:
                         heure %= 24
 
             if affichage == "AMPM":
-                am_pm_indicator = "PM" if heure >= 12 else "AM"
+                indicateur_am_pm = "PM" if heure >= 12 else "AM"
                 heure_affichage = heure if heure <= 12 else heure - 12
                 if heure_affichage == 0:
                     heure_affichage = 12
 
-                print(f"\r\033[K{heure_affichage:02d}:{minute:02d}:{seconde:02d} {am_pm_indicator}", end="", flush=True)
+                print(f"\r\033[K{heure_affichage:02d}:{minute:02d}:{seconde:02d} {indicateur_am_pm}", end="", flush=True)
             else:
                 print(f"\r\033[K{heure:02d}:{minute:02d}:{seconde:02d}", end="", flush=True)
 
-        dalarme(heure_alarme, minute_alarme, seconde_alarme, heure, minute, seconde)
-
         sleep(1)
 
+# Paramètres initiaux de l'heure
+heure_base = (13, 30, 0)
+heure_alarme = (13, 30, 5)
+est_en_pause = False
 
-heure = (13, 30, 0)
-halarme = (13, 30, 10)
-afficher_heure(heure, halarme)
+# Démarrer l'horloge
+afficher_heure(heure_base, heure_alarme, est_en_pause)
